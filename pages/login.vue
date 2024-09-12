ed3718d457584ce7b903b9438cb18dae8f544f25
ranandrej <ranandrej@gmail.com>
2024-09-12 14:36:24 +0200

    Improved forms

--- pages/login.vue
+++ pages/login.vue
@@ -75,19 +75,78 @@ const resetSifra=async()=>{
 <template>
 <Loading v-if="loading"/>
 <SuccessAlert v-if="resetSifraSuccess" :message="`Link sa promenu sifre poslat na e-mail ${email}`"/>
-<section class="md:p-10 py-6 w-full bg-[url('~/assets/bacground1.jpg')] bg-cover">
-  <div class="mx-auto w-[80%] md:w-1/3 p-8 bg-slate-800 flex flex-wrap justify-start">
-    <h2 class="text-white text-lg m-2 w-full">Ulogujte se</h2>
-    <p v-if="errorMessage" class="text-sm w-full text-red-500">{{ errorMessage }}</p>
-    <input class="m-2 w-full rounded-md p-1 text-black" type="email" v-model="email" placeholder="E-mail"/>
-    <input class="m-2 w-full rounded-md p-1 text-black" type="password" v-model="password" placeholder="Lozinka"/>
-    <button class="m-2 bg-transparent border-2 border-yellow-500  p-2 cursor-pointer hover:bg-yellow-500 text-white" @click="logIn()">Prijava</button>
-    
-    <p class="text-sm w-full text-white">Nemate nalog?<NuxtLink class="text-yellow-500" to="/register">Registrujte se</NuxtLink></p>
-    <p v-if="showReset" class="text-sm w-full text-white">Zaboravili ste lozinku?<button class="text-yellow-500 cursor-pointer bg-transparent" @click="resetSifra">Kliknite za reset</button></p>
+<section class="h-full flex items-center justify-center bg-[url('~/assets/bacground1.jpg')] bg-cover py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8 bg-slate-800 p-8 rounded-lg shadow-xl">
+      <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-white">
+          Ulogujte se
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" @submit.prevent="logIn">
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              v-model="email"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              placeholder="E-mail"
+            />
+          </div>
+          <div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              v-model="password"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              placeholder="Lozinka"
+            />
+          </div>
+        </div>
 
-  </div>
-  
-</section>
+        <div v-if="errorMessage" class="flex items-center text-red-500 text-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+          </svg>
+          <span>{{ errorMessage }}</span>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+          >
+            Prijava
+          </button>
+        </div>
+      </form>
+
+      <div class="flex items-center justify-between">
+        <div class="text-sm">
+          <p class="text-gray-300">
+            Nemate nalog?
+            <NuxtLink to="/register" class="font-medium text-yellow-500 hover:text-yellow-400">
+              Registrujte se
+            </NuxtLink>
+          </p>
+        </div>
+        <div class="text-sm">
+          <button 
+            v-if="showReset"
+            @click="resetSifra"
+            class="font-medium text-yellow-500 hover:text-yellow-400"
+          >
+            Zaboravili ste lozinku?
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
 
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -60,22 +60,117 @@ async function signUp() {
 <template>
 <Loading v-if="loading"/>
 <SuccessAlert v-if="registerSuccess" :message="`E-mail potvrde poslat na ${email}!`"/>
-<section class="md:p-10 py-6 w-full  bg-[url('~/assets/bacground1.jpg')] bg-cover">
-  <form @submit.prevent="signUp()" class="mx-auto w-[80%] md:w-1/3 p-8 bg-slate-800 flex flex-wrap justify-start">
-    <h2 class="text-white text-lg m-2 w-full">Kreirajte nalog</h2>
-    <p class="text-yellow-500 text-xs">*sva polja obavezna!</p>
-    <p v-if="errorMessage" class="text-sm w-full text-red-500">{{ errorMessage }}</p>
-    <input class="m-2 w-full rounded-md p-1 text-black" required type="text" maxlength="20" v-model="ime" placeholder="Vaše ime"/>
-    <input class="m-2 w-full rounded-md p-1 text-black" required type="email" v-model="email" placeholder="E-mail"/>
-    <input class="m-2 w-full rounded-md p-1 text-black" required type="password" v-model="password" placeholder="Lozinka"/>
-    <input class="m-2 w-full rounded-md p-1 text-black" required type="text" maxlength="20" v-model="telefon" placeholder="Telefon"/>
-    <div class="flex items-center mb-4">
-        <input type="checkbox" id="terms" v-model="acceptTerms" class="mr-2"/>
-        <label for="terms" class="text-white">Prihvatam <NuxtLink to="/uslovi-koriscenja" class="text-blue-400 underline cursor-pointer">uslove korišćenja</NuxtLink></label>
-      </div>
-    <button class="m-2 bg-transparent border-2 w-full border-yellow-500 p-2 cursor-pointer hover:bg-yellow-500 text-white" type="submit">Registracija</button>
+
+<div class="h-full relative flex items-center justify-center bg-[url(~/assets/background3.jpg)] bg-center py-2 px-4 sm:px-6 lg:px-8">
+  <div class="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-70 z-0"></div>
+
+  <div class="relative max-w-md w-full space-y-8">
     
-    <p class="text-sm w-full text-white">Već imate nalog?<NuxtLink class="text-yellow-500" to="/login">Ulogujte se</NuxtLink></p>
-  </form>
-</section>
+      <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-white">
+          Kreirajte nalog
+        </h2>
+        <p class="mt-2 text-center text-sm text-yellow-500">
+          *sva polja obavezna!
+        </p>
+      </div>
+      <form class="mt-8 space-y-6" @submit.prevent="signUp">
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              required
+              maxlength="20"
+              v-model="ime"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              placeholder="Vaše ime"
+            />
+          </div>
+          <div>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              v-model="email"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              placeholder="E-mail"
+            />
+          </div>
+          <div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              v-model="password"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              placeholder="Lozinka"
+            />
+          </div>
+          <div>
+            <input
+              id="phone"
+              name="phone"
+              type="text"
+              required
+              maxlength="20"
+              v-model="telefon"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+              placeholder="Telefon"
+            />
+          </div>
+        </div>
+
+        <div class="flex items-center">
+          <input
+            id="terms"
+            name="terms"
+            type="checkbox"
+            v-model="acceptTerms"
+            class="h-4 w-4 text-yellow-600 focus:ring-yellow-500 border-gray-300 rounded"
+          />
+          <label for="terms" class="ml-2 block text-sm text-gray-300">
+            Prihvatam
+            <NuxtLink to="/uslovi-koriscenja" class="text-yellow-500 hover:text-yellow-400">
+              uslove korišćenja
+            </NuxtLink>
+          </label>
+        </div>
+
+        <div v-if="errorMessage" class="flex items-center text-red-500 text-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+          </svg>
+          <span>{{ errorMessage }}</span>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+          >
+            Registracija
+          </button>
+        </div>
+      </form>
+      <div class="flex flex-wrap text-white">
+        
+        <p class="w-full"><span class="mx-1 text-green-500"><i class="bi bi-check2-circle"></i></span>Jednostavno postavljanje oglasa</p>
+        <p class="w-full"><span class="mx-1 text-green-500"><i class="bi bi-check2-circle"></i></span>Brza i laka registracija</p>
+        <p class="w-full"><span class="mx-1 text-green-500"><i class="bi bi-check2-circle"></i></span>Mogućnost upravljanja oglasima</p>
+
+      </div>
+      <p class="mt-2 text-center text-sm text-gray-300">
+        Već imate nalog?
+        <NuxtLink to="/login" class="font-medium text-yellow-500 hover:text-yellow-400">
+          Ulogujte se
+        </NuxtLink>
+      </p>
+    </div>
+  </div>
 </template>
