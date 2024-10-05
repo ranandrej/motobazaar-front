119f773ff5cf59e5a5e4965e18c54bacced4247c
ranandrej <ranandrej@gmail.com>
2024-10-05 11:53:09 +0200

    Update SEO

--- app.vue
+++ app.vue
@@ -2,11 +2,15 @@
 import { useHead } from 'nuxt/app'
 
 useHead({
-  title: 'MotoBazaar - Vaša platforma za motocikle',
+  title: 'Oglasi za motocikle | MotoBazaar - Kupujte i Prodajte Polovne Motocikle',
   meta: [
     {
       name: 'description',
       content: 'Pronađite najbolje motocikle, pretražite oglase i kupite svoj savršeni motor na MotoBazaar-u. Bezbedno i jednostavno, povezujemo ljubitelje motocikala sa prodavcima.'
+    },
+    {
+      name: 'keywords',
+      content: 'polovan motocikl, motocikli na prodaju, kupovina motora, prodaja motocikala, MotoBazaar, oglasi za motocikle,polovni motocikli'
     }
   ]
 })
